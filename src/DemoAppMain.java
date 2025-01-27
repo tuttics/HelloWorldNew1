86ff36c6181328c2f6c3725ed5ee85186f25c9f3
tuttics <52403815@edu.campus02.at>
2025-01-27 21:33:22 +0100

    Initial commit

---
+++ src/DemoAppMain.java
@@ -0,0 +1,23 @@
+public class DemoAppMain {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+
+        System.out.println("Hello World! Das hier ist mein Kalkulator.");
+
+        System.out.println("Wir berechnen 5 * 7");
+        System.out.println();
+
+        int result = a * b;
+        if (result > 30){
+            System.out.println("result over 30");
+        } else {
+            System.out.println("result below 30");
+        }
+
+        System.out.println();
+
+        System.out.println("Zweite Zeile");
+
+    }
+}
